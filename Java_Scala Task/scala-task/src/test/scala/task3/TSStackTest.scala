9c51281059e721af2380ec6b15757158781cad0a
SGERASIMOV <sgerasimov@griddynamics.com>
2018-04-01 12:06:39 +0300

    task3: add TSStack units

---
+++ Java_Scala Task/scala-task/src/test/scala/task3/TSStackTest.scala
@@ -0,0 +1,56 @@
+package task3
+
+import org.scalatest.{FreeSpec, Matchers}
+
+class TSStackTest extends FreeSpec with Matchers {
+
+  class EmptyStackFixture {
+    val stack: TSStack[Int] = Empty
+  }
+
+  class StackWithOneElementFixture {
+    val stack: TSStack[Int] = Cons(1, Empty)
+  }
+
+  "check stack isEmpty operation" in new EmptyStackFixture {
+    assert(stack.isEmpty)
+  }
+
+  "should produce NoSuchElementException when head is invoked" in new EmptyStackFixture {
+    intercept[NoSuchElementException](stack.head)
+  }
+
+  "should produce NoSuchElementException when tail is invoked" in new EmptyStackFixture {
+    intercept[NoSuchElementException](stack.tail)
+  }
+
+  "should produce NoSuchElementException after push, tail and tail are invoked" in new EmptyStackFixture {
+    intercept[NoSuchElementException](stack.push(2).tail.tail)
+  }
+
+  "toString on Empty stack: TSStack()" in new EmptyStackFixture {
+    assert(stack.toString == "TSStack()")
+  }
+
+  "check isEmpty operation on non-Empty stack" in new StackWithOneElementFixture {
+    assert(!stack.isEmpty)
+  }
+
+  "check push-head operations on non-Empty stack" in new StackWithOneElementFixture {
+    assert(stack.push(3).head == 3)
+    assert(stack.push(4).head == 4)
+  }
+
+  "check tail operation on non-Empty stack" in new StackWithOneElementFixture {
+    assert(stack.push(2).tail == Cons(1, Empty))
+  }
+
+  "toString with 1 element" in new StackWithOneElementFixture {
+    assert(stack.toString == "TSStack(1)")
+  }
+
+  "toString with 2 elements" in new StackWithOneElementFixture {
+    assert(stack.push(2).toString == "TSStack(2, 1)")
+  }
+
+}
